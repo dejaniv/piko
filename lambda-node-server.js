fdb6762f7022c97279588bda99c6e1d41b31a57c
Dejan Ivkovic <dejaniv@gmail.com>
2017-05-11 16:36:42 -0700

    S3 bucket name

--- lambda-node-server.js
+++ lambda-node-server.js
@@ -31,7 +31,7 @@ const CONTENT_DIR = "content";
 const IMAGES_DIR = "images";
 const TEMPLATE_FILE = "template.html";
 const MAX_URL_LENGTH = 128;
-const CONTENT_BUCKET = "yours3bucker"
+const CONTENT_BUCKET = "yours3bucket"
 
 function getContentTree(dir, done) {
     var tree = {};
